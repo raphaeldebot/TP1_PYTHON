afebff5c85a5c1099c81e350dcdbfa47030a2778
raphaeldebot <debot.raphael@gmail.com>
2026-02-19 22:05:24 +0100

    EXO15 done

---
+++ EXO15.py
@@ -0,0 +1,4 @@
+prenom = input("Entrez votre prenom : ")# Je récupère le nom de l'utilisateur dans une variable
+nom = input("Entrez votre nom : ")# Maintenant je récupère son nom dans une deuxieme variable
+
+print(f"Vous vous appellez donc {prenom} {nom}.")# Et la je lui renvoy son prenom et nom comme si j'étais big brother
